0b9889d5af5ce65b401d034d92ce71cdb15babbd
Ha Thu <haathu1908@gmail.com>
2024-08-30 21:27:17 +0700

    lab1random

---
+++ app/src/main/java/com/example/lab1bai2/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.lab1bai2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewDice;
+    private Button buttonRoll;
+
+    private int[] diceArray = {
+            R.drawable.dice1,
+            R.drawable.dice2,
+            R.drawable.dice3,
+            R.drawable.dice4,
+            R.drawable.dice5,
+            R.drawable.dice6
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageViewDice = findViewById(R.id.imageViewDice);
+        buttonRoll = findViewById(R.id.buttonRoll);
+
+        buttonRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollDice();
+            }
+        });
+    }
+
+    private void rollDice() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(6);
+
+        imageViewDice.setImageResource(diceArray[randomNumber]);
+    }
+}
